a5286ea6ad569cd80fe975ebce4e9a9a616fc79d
Kartik014 <kartikagarwal014@gmail.com>
2025-03-03 01:45:58 +0530

    Implemented JWT token generation and logIn api endpoint

--- src/main/kotlin/com/sharedoc/shareDoc/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/sharedoc/shareDoc/configuration/SecurityConfig.kt
@@ -1,11 +1,7 @@
 package com.sharedoc.shareDoc.configuration
 
-import com.sharedoc.shareDoc.services.UserService
-import com.sharedoc.shareDoc.utils.CustomAuthenticationProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
