661513faa83313b01e4e9bf3778cf70e61696ddb
Kartik014 <kartikagarwal014@gmail.com>
2025-03-02 15:19:08 +0530

    Initial commit

---
+++ src/test/kotlin/com/sharedoc/shareDoc/ShareDocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sharedoc.shareDoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShareDocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
